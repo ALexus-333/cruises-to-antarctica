b5d5c59a20e5215c0d25d5cf59f5ec1ce2e4c839
Aleksey Yakovlev <volkracing555@gmail.com>
2023-04-24 14:26:37 +0300

    Готовый сайт №7

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {menu, hidesMenu, hidesMenuOverlay} from './modules/menu';
+import {initMenu} from './modules/menu';
 
 // ---------------------------------
 
@@ -16,9 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    menu();
-    hidesMenu();
-    hidesMenuOverlay();
+    initMenu();
   });
 });
 
--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -1,25 +1,30 @@
 const mainHeader = document.querySelector('.main-header');
-const mainNav = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
-const headerLogo = document.querySelector('.main-header__link-logo');
-const mainBlock = document.querySelector('.main-block__wrapper');
-const navList = document.querySelector('.main-nav__list');
-const headerWrapper = document.querySelector('.main-header__wrapper');
-const navWrapper = document.querySelector('.main-nav__wrapper');
 
-mainHeader.classList.remove('main-header--nojs');
-mainNav.classList.remove('main-nav--nojs');
+export const initMenu = () => {
+  if (!mainHeader) {
+    return;
+  }
 
-// Функция закрытия меню
-const closesMenu = () => {
-  mainNav.classList.add('main-nav--closed');
-  mainNav.classList.remove('main-nav--opened');
-  headerLogo.classList.remove('main-header__link-logo--opened-mobile');
-  mainBlock.classList.remove('main-block__wrapper--opened-mobile');
-  document.body.style.overflow = '';
-};
+  const mainNav = document.querySelector('.main-nav');
+  const navToggle = document.querySelector('.main-nav__toggle');
+  const headerLogo = document.querySelector('.main-header__link-logo');
+  const mainBlock = document.querySelector('.main-block__wrapper');
+  const navList = document.querySelector('.main-nav__list');
+  const headerWrapper = document.querySelector('.main-header__wrapper');
+  const navWrapper = document.querySelector('.main-nav__wrapper');
 
-const menu = () => {
+  mainHeader.classList.remove('main-header--nojs');
+  mainNav.classList.remove('main-nav--nojs');
+
+  // Функция закрытия меню
+  const closeMenu = () => {
+    mainNav.classList.add('main-nav--closed');
+    mainNav.classList.remove('main-nav--opened');
+    headerLogo.classList.remove('main-header__link-logo--opened-mobile');
+    mainBlock.classList.remove('main-block__wrapper--opened-mobile');
+    document.body.style.overflow = '';
+  };
+
   navToggle.addEventListener('click', () => {
     if (mainNav.classList.contains('main-nav--closed')) {
       mainNav.classList.remove('main-nav--closed');
@@ -29,28 +34,20 @@ const menu = () => {
       document.body.style.overflow = 'hidden';
       navWrapper.style.overflow = 'auto';
     } else {
-      closesMenu();
+      closeMenu();
     }
   });
-};
-
-// Скрывает в mobile меню при нажатии на ссылку
-const hidesMenu = () => {
+  // Скрывает в mobile меню при нажатии на ссылку
   navList.addEventListener('click', () => {
     if (mainNav.classList.contains('main-nav--opened')) {
-      closesMenu();
+      closeMenu();
     }
   });
-};
-
-// Скрывает в mobile меню при нажатии на overlay
-const hidesMenuOverlay = () => {
+  // Скрывает в mobile меню при нажатии на overlay
   headerWrapper.addEventListener('click', (evt) => {
     let target = evt.target;
     if (mainNav.classList.contains('main-nav--opened') && !target.classList.contains('main-nav__wrapper') && !target.classList.contains('main-nav__toggle')) {
-      closesMenu();
+      closeMenu();
     }
   });
 };
-
-export {menu, hidesMenu, hidesMenuOverlay};
